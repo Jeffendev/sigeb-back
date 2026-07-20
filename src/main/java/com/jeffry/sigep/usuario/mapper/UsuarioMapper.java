feff8fb18a89fc5af1198d8109f5eff84a2f3e1d
Jeffry Arrieta <jeffryunad@gmail.com>
2026-07-20 11:09:36 -0500

    feat:mapper

---
+++ src/main/java/com/jeffry/sigep/usuario/mapper/UsuarioMapper.java
@@ -0,0 +1,29 @@
+package com.jeffry.sigep.usuario.mapper;
+
+import com.jeffry.sigep.usuario.dto.UsuarioRequestDTO;
+import com.jeffry.sigep.usuario.dto.UsuarioResponseDTO;
+import com.jeffry.sigep.usuario.entity.Usuario;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UsuarioMapper {
+
+    /**
+     * Convierte UsuarioRequestDTO a Usuario.
+     */
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "activo", ignore = true)
+    @Mapping(target = "ultimoAcceso", ignore = true)
+    @Mapping(target = "fechaCreacion", ignore = true)
+    @Mapping(target = "fechaActualizacion", ignore = true)
+    @Mapping(target = "rol", ignore = true)
+    Usuario toEntity(UsuarioRequestDTO dto);
+
+    /**
+     * Convierte Usuario a UsuarioResponseDTO.
+     */
+    @Mapping(target = "rol", source = "rol.nombre")
+    UsuarioResponseDTO toResponse(Usuario usuario);
+
+}
